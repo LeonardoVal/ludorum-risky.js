5f0d9c0501ecb8f0e6a8dbd2c68270de7c906d9f
Leonardo Val <leonardo.val@creatartis.com>
2017-11-04 20:17:28 -0300

    Playtester
    + SVG based playtester. Still WIP.

---
+++ tests/risky.js
@@ -0,0 +1,42 @@
+require(['require-config'], function (init) { "use strict";
+init(['ludorum-risky', 'ludorum', 'creatartis-base', 'sermat', 'playtester'],
+function (ludorum_risky, ludorum, base, Sermat, PlayTesterApp) {
+
+	base.HttpRequest.get('maps/map-test01.svg').then(function (xhr) {
+		document.getElementById('board').innerHTML = xhr.response;
+
+		/** PlayTesterApp initialization.
+		*/
+		base.global.APP = new PlayTesterApp(
+			new ludorum_risky.Risk({
+				boardMap: ludorum_risky.maps.test01,
+				armies: {
+					WhiteCountry:  ['White', 6],
+					YellowCountry: ['Yellow', 6],
+					RedCountry:    ['Red', 6],
+					GreenCountry:  ['Green', 6],
+					BlueCountry:   ['Blue', 6],
+					BlackCountry:  ['Black', 6],
+				}
+		 	}),
+			new ludorum_risky.RiskSVGInterface(),
+			//new ludorum.players.UserInterface.BasicHTMLInterface({ container: document.getElementById('board') }),
+			{ bar: document.getElementsByTagName('footer')[0] });
+		APP.playerUI("You")
+			.playerRandom()
+			/*
+			.playerMonteCarlo("MCTS (10 sims)", true, 10)
+			.playerMonteCarlo("MCTS (100 sims)", true, 100)
+			.playerUCT("UCT (10 sims)", true, 10)
+			.playerUCT("UCT (100 sims)", true, 100)
+			.playerAlfaBeta("MiniMax-\u03b1\u03b2 (4 plies)", true, 3)
+			.playerAlfaBeta("MiniMax-\u03b1\u03b2 (6 plies)", true, 5)
+			.playerMaxN("MaxN (6 plies)", true, 5)
+			*/
+			.selects(['playerWhite', 'playerYellow', 'playerRed', 'playerGreen', 'playerBlue',
+				'playerBlack'])
+			.button('resetButton', document.getElementById('reset'), APP.reset.bind(APP))
+			.reset();
+	}); // HttpRequest.get
+}); // init()
+}); // require().
