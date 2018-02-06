3434fd110086938185f65cd1fae748cb5fc6837a
Leonardo Val <leonardo.val@creatartis.com>
2018-02-06 12:08:41 -0300

    New build

--- build/ludorum-risky.js
+++ build/ludorum-risky.js
@@ -1155,7 +1155,10 @@ var Risk = exports.Risk = declare(Game, {
 	'static __SERMAT__': {
 		identifier: 'Risk',
 		serializer: function serialize_Risk(obj) {
-			return this.serializeAsProperties(obj, ['boardMap', 'stage', 'round', 'rounds', 'armies'], true);
+			var args = this.serializeAsProperties(obj, 
+				['boardMap', 'stage', 'round', 'rounds', 'armies'], true);
+			args[0].activePlayer = obj.activePlayer();
+			return args;
 		}
 	},
 
