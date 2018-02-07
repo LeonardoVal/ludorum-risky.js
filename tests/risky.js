55f976bc8693ed998fa669d7689eef9eedb068de
Leonardo Val <leonardo.val@creatartis.com>
2018-02-06 22:06:46 -0300

    Playtester + Added buttons to set all players to Random and MCTS(10). + Game with placement fails if armies are in place, and finishes immediately if the board is empty.

--- tests/risky.js
+++ tests/risky.js
@@ -11,6 +11,7 @@ function (ludorum_risky, ludorum, base, Sermat, PlayTesterApp) {
 		base.global.APP = new PlayTesterApp(
 			new ludorum_risky.Risk({
 				boardMap: ludorum_risky.maps.test01,
+				//stage: [-1],
 				armies: {
 					WhiteCountry:  ['White', 6],
 					YellowCountry: ['Yellow', 6],
@@ -31,8 +32,19 @@ function (ludorum_risky, ludorum, base, Sermat, PlayTesterApp) {
 			.playerUCT("UCT (100 sims)", true, 100)
 			.selects(['playerWhite', 'playerYellow', 'playerRed', 'playerGreen', 'playerBlue',
 				'playerBlack'])
-			.button('resetButton', document.getElementById('reset'), APP.reset.bind(APP))
-			.reset();
+			.button('resetButton', document.getElementById('reset'), APP.reset.bind(APP));
+		
+		APP.setAllPlayers = function setAllPlayers(p) {
+			this.elements.selects.forEach(function (select) {
+				select.value = p;
+				select.onchange();
+			});
+			this.reset();
+		};
+		document.getElementById('all-random').onclick = APP.setAllPlayers.bind(APP, 1);
+		document.getElementById('all-MCTS10').onclick = APP.setAllPlayers.bind(APP, 2);
+
+		APP.reset();
 	}); // HttpRequest.get
 }); // init()
 }); // require().
